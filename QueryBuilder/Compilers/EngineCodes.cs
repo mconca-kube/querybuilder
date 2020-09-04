632614b2dd97afc5f4668e731383f41b92258f58
Marco Conca <mconca@kubesistemi.it>
2020-09-04 18:25:04 +0200

    Added support for SQL Anywhere

--- QueryBuilder/Compilers/EngineCodes.cs
+++ QueryBuilder/Compilers/EngineCodes.cs
@@ -9,5 +9,6 @@
         public const string PostgreSql = "postgres";
         public const string Sqlite = "sqlite";
         public const string SqlServer = "sqlsrv";
+        public const string SqlAnywhere = "sqlanywhere";    // MC 2020.09.04 New Compiler For SQL Anywhere Database
     }
 }
