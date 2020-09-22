dab121760eacbd03cd4bc5c02f9e6584210743c2
Marco Conca <mconca@kubesistemi.it>
2020-09-22 11:03:29 +0200

    Gestione nuget BaGet

---
+++ QueryBuilder/UploadBaGet.bat
@@ -0,0 +1,14 @@
+set progetto=SqlKataKube
+
+del /s %progetto%.*.*nupkg
+dotnet build -c DEBUG
+dotnet pack -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg
+for /f "delims=" %%a in ('dir /b /aa /on "bin\Debug\%progetto%.*.nupkg"') do set ver=%%a
+nuget push -Source "http://192.168.3.114:5000/v3/index.json" bin\Debug\%ver%
+
+
+ECHO FINITO!
+pause
+
+REM Se ci sono più progetti nella stessa cartella:
+REM dotnet pack %progetto%.csproj -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg
