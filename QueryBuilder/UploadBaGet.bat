b7436793ddcd54f0f2660b2a7e57cc93a7af113c
Marco Conca <mconca@kubesistemi.it>
2021-04-09 12:01:42 +0200

    Aggiornata versione nuget

--- QueryBuilder/UploadBaGet.bat
+++ QueryBuilder/UploadBaGet.bat
@@ -1,7 +1,7 @@
 set progetto=SqlKataKube
 
 del /s %progetto%.*.*nupkg
-dotnet build -c DEBUG
+dotnet build -c RELEASE
 dotnet pack -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg
 for /f "delims=" %%a in ('dir /b /aa /on "bin\Debug\%progetto%.*.nupkg"') do set ver=%%a
 nuget push -Source "http://192.168.3.114:5000/v3/index.json" bin\Debug\%ver%
