2d36d204a7a395868b90620e96f3658ef413f897
Marco Conca <mconca@kubesistemi.it>
2021-11-29 16:21:36 +0100

    Aggiornamenti nuget vari

--- QueryBuilder/UploadBaGet.bat
+++ QueryBuilder/UploadBaGet.bat
@@ -1,14 +1,13 @@
 set progetto=SqlKataKube
 
 del /s %progetto%.*.*nupkg
-dotnet build -c RELEASE
 dotnet pack -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg
 for /f "delims=" %%a in ('dir /b /aa /on "bin\Debug\%progetto%.*.nupkg"') do set ver=%%a
-nuget push -Source "http://192.168.3.114:5000/v3/index.json" bin\Debug\%ver%
+nuget push -Source "http://192.168.3.110:5000/v3/index.json" bin\Debug\%ver%
 
 
 ECHO FINITO!
 pause
 
-REM Se ci sono più progetti nella stessa cartella:
+REM Se ci sono piÃ¹ progetti nella stessa cartella:
 REM dotnet pack %progetto%.csproj -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg
